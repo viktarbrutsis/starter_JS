cb163c9ce2d6190c4e3e232ea61dd9dadd0bc710
Viktar <viktarbrutsis@gmail.com>
2022-10-13 16:58:31 +0300

    started lesson19

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//console.log('Padla');
+
+// const obj = {
+//     name: 'Viktar',//свойство обьекта
+//     age: 42,
+//     isMarried: true
+// };
+
+//  console.log(obj.name);//так лучше!!!
+//  console.log(obj['age']);
+
+//  const arr = [1, 2, 3];
+//  const object = {
+//     'Anna': 400,
+//      Alice: 450 //можно ключ писать в кавычках и без
+//  };
+
+ //добавить свойство к объекту
+
+ //object.Inna = 390;
+//  object['Inna'] = 390;
+//  console.log(object);
+
+//  const answer = confirm('Are you here?');
+//  console.log(answer);
+// const answer = +prompt('Are you 19 years old?', '18');
+// console.log(typeof(answer));
+
+// const category = 'toys';
+// console.log(`https://someurl/${category}/5`);
+
+const user = 'Viktor';
+console.log(`Привет, ${user}`);
