763d46c20fb9d376594a92e971e39a7fbaeaf7ef
Viktar <viktarbrutsis@gmail.com>
2022-10-17 15:57:32 +0300

    lesson 29 done

--- js/lesson_19.js
+++ js/lesson_19.js
@@ -22,9 +22,44 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+// if (numberOfFilms < 10) {
+//     alert("Просмотрено довольно мало фильмов");
+// } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+//     alert("Вы классический зритель");
+// } else if (numberOfFilms > 30) {
+//     alert("Вы киноман");
+// } else {
+//     alert("Произошла ошибка");
+// }
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        alert('Введите корректные данные!!!');
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,9 +68,62 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const answer1 = prompt('Один из последних просмотренных фильмов?', '');
-const answer2 = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies.answer1 = answer2;
+function myFilms() {
+    for (let i = 1; i <= 2; i++) {
+        const answer1 = prompt('Один из последних просмотренных фильмов?', '');
+        const answer2 = +prompt('На сколько оцените его?', '');
+    
+        if (answer1 === '' || answer1 === null || answer1.length > 50 || answer2 === '' || answer2 === null) {
+            alert('Введите корректные данные!!!');
+            i--;
+            } else {
+                personalMovieDB.movies[answer1] = answer2;
+                alert('Done!!!');
+            }
+        
+        
+    }
+}
 
-console.log(personalMovieDB);
+myFilms();
+
+function writetYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+
+writetYourGenres();
+
+
+function check() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && numberOfFilms.count <= 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+
+check();
+
+// function showMyDB() {
+//     if (!personalMovieDB.privat) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
--- js/script.js
+++ js/script.js
@@ -1,35 +1,158 @@
 'use strict';
 
-//console.log('Padla');
+loop_1: for (let i = 1; i < 4; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 1; j < 4; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 1; k < 4; k++) {
+            if (k === 3) continue loop_1;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+for (let k = 2; k <= 10; k++) {
+    if (k%2 === 0) {
+        console.log(k);
+    }
+}
+let l = 2;
+while (l <= 10) {
+        if (l%2 === 0) {
+            console.log(l);
+        }
+        l++;
+    }
+let arrayOfNumbers = [];
+for (let i = 5; i <= 10; i++)  {
+    arrayOfNumbers[i - 5] = i;
+}
+console.log(arrayOfNumbers);
 
-// const obj = {
-//     name: 'Viktar',//свойство обьекта
-//     age: 42,
-//     isMarried: true
-// };
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for ( let i = 0; i < data.length; i++) {
+//     result[i] = data[data.length - 1 - i];
+// }
+// console.log(result);
 
-//  console.log(obj.name);//так лучше!!!
-//  console.log(obj['age']);
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+//     for (let o = 0, o < arr.length; o++) {
+//         result[o] = arr[o];
+//     }
+// console.log(result);
 
-//  const arr = [1, 2, 3];
-//  const object = {
-//     'Anna': 400,
-//      Alice: 450 //можно ключ писать в кавычках и без
-//  };
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for (let j = 0; j < data.length; j++) {
+//     if (typeof(data[j]) == 'string') {
+//         data[j] = data[j] + ' -done';
+//     } else {
+//         data[j] = data[j] * 2;
+//     }
+// }
+// console.log(data);
 
- //добавить свойство к объекту
+// const lines = 5;
+// let result = '';
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//         }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//         }
+//     result += "\n";
+// }
+// console.log(result);
 
- //object.Inna = 390;
-//  object['Inna'] = 390;
-//  console.log(object);
+/* Задание на урок:
 
-//  const answer = confirm('Are you here?');
-//  console.log(answer);
-// const answer = +prompt('Are you 19 years old?', '18');
-// console.log(typeof(answer));
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-// const category = 'toys';
-// console.log(`https://someurl/${category}/5`);
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-const user = 'Viktor';
-console.log(`Привет, ${user}`);
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+// Код возьмите из предыдущего домашнего задания
+// let val = 7;
+// function createAdder() {
+//     function addNumbers(a, b) {
+//     let ret = a + b;
+//     //return ret;
+//     }
+//     return addNumbers;
+// }
+// let adder = createAdder();
+// let sum = adder(val, 8);
+// console.log('example of function returning a function: ', sum);
+
+// let num = 20;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;// объявили переменную локально то есть внутри функции 
+// }
+// showFirstMessage('Hello World!!!');
+// console.log(num);// 20 так как локальная переменная не видна
+
+//  function ret() {
+//     let num = 50;
+//     return num;
+//  }
+//  const number = ret();
+//  console.log(number);
+
+// const calc = (a, b) => a + b;
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert (amount, rate) {
+//     return amount * rate;
+// }
+// function promotion (result) {
+//     console.log(result * discount);
+// } 
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+function getMathResult (a, b) {
+    if (b <= 0 || typeof(b) !== 'number') {
+        return a;
+    }
+        else {
+            let str = '';
+            for (let i = 1; i <= b; i++) {
+                if ( i === b) {
+                    str = str + a * i;
+                } else {
+                    str = str + a * i + '---';
+                }
+               
+            }
+            return str;
+        }
+    }
+    console.log(getMathResult(4, 5));
+
+
+    function readNumber() {
+        let num;
+      
+        do {
+          num = prompt("Введите число", 0);
+        } while ( !isFinite(num) );
+      
+        if (num === null || num === '') return null;
+      
+        return +num;
+      }
+      
+      alert(`Число: ${readNumber()}`);
+      
